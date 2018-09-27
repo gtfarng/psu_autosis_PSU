0682e8b47fffe0e7d92b1cc564338e5ce3a5532a
gtfarng <5735512002@psu.ac.th>
2018-09-27 08:46:42 +0700

    test

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$PSU_Passport_Username = ''; // PSU Passport Username
+$PSU_Passport_Password = ''; // PSU Passport Password
+$SIS_Term = ''; // Semester for register
+$Subject_To_Register = array(
+	array('SubjectCode' => '', 'SubjectSec' => '', 'SubjectCredit' => ''), // Subject for register
+); 
+$SIS_URL = ''; // PSU SIS URL
+
+?>
